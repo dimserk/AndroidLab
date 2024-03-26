f98d05d9429875bbf31dd3f0ba98b62b71309a90
Дима Куликов <dimserk@yandex.ru>
2024-03-26 17:31:33 +0300

    Add snackbar buttons

--- app/src/main/java/aaa/dimserk/androidlab/MainActivity.kt
+++ app/src/main/java/aaa/dimserk/androidlab/MainActivity.kt
@@ -1,25 +1,34 @@
 package aaa.dimserk.androidlab
 
+import aaa.dimserk.androidlab.ui.theme.AndroidLabTheme
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import aaa.dimserk.androidlab.ui.theme.AndroidLabTheme
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -43,43 +52,110 @@ fun Greeting(
     modifier: Modifier = Modifier,
     viewModel: UIViewModel = viewModel()
 ) {
+    val context = LocalContext.current
     val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    var visible by remember { mutableStateOf(false) }
+    var longTaskEnabled by remember { mutableStateOf(true) }
+    var progressCardVisible by remember { mutableStateOf(false) }
 
-    if (visible) {
+    if (progressCardVisible) {
         ProgressCard(
-            onDissmiss = { visible = false },
-            onCancel = { }
+            onDissmiss = { progressCardVisible = false },
+            modifier = modifier,
         )
     }
 
-    Column {
-        Row {
-            Text(
-                text = viewModel.getHelloMessage(),
-                modifier = modifier
-            )
-        }
-
-        Button(onClick = {
-            scope.launch {
-                Log.d("TAG", "started")
-                visible = true
-                viewModel.longTask()
-                visible = false
-                Log.d("TAG", "done")
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        modifier = modifier,
+    ) { innerPadding ->
+        Column(
+            modifier = modifier.padding(innerPadding),
+        ) {
+            Row(
+                modifier = modifier,
+            ) {
+                Text(
+                    text = viewModel.getHelloMessage(),
+                    modifier = modifier,
+                )
             }
-        }) {
-            Text(text = "Push me")
+
+            Button(
+                enabled = longTaskEnabled,
+                modifier = modifier,
+                onClick = {
+                    scope.launch {
+                        Logger.verbose ("Long task started")
+                        longTaskEnabled = false
+                        progressCardVisible = true
+                        viewModel.longTask()
+                        progressCardVisible = false
+                        Logger.verbose("Long task done")
+                        longTaskEnabled = true
+                    }
+                }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.start_long_task_button),
+                )
+            }
+
+            Text(
+                text = stringResource(id = R.string.snackbars_label),
+                modifier = modifier,
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                modifier = modifier.fillMaxWidth(),
+            ) {
+                Button(
+                    modifier = modifier,
+                    onClick = {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = context.getString(R.string.snackbar_massage),
+                            withDismissAction = true
+                        )
+                    }
+                }) {
+                    Text(
+                        text = stringResource(id = R.string.show_snackbar),
+                        modifier = modifier,
+                    )
+                }
+
+                Button(
+                    modifier = modifier,
+                    onClick = {
+                        scope.launch {
+                            val result = snackbarHostState.showSnackbar(
+                                message = context.getString(R.string.snackbar_massage),
+                                actionLabel = context.getString(R.string.snackbar_action_label),
+                                withDismissAction = true,
+                                duration = SnackbarDuration.Long
+                            )
+
+                            when (result) {
+                                SnackbarResult.ActionPerformed -> {
+                                    Logger.verbose("Action perfomed")
+                                }
+
+                                SnackbarResult.Dismissed -> {
+                                    Logger.verbose("Action dismissed")
+                                }
+                            }
+                        }
+                    }
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.show_action_snackbar),
+                        modifier = modifier,
+                    )
+                }
+            }
         }
     }
 }
-
-/*@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidLabTheme {
-        Greeting()
-    }
-}*/
