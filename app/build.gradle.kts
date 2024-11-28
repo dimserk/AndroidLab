28a917b323ee43a56d28da30ca7a527e1a9b886c
dima <mail@mail.org>
2024-11-28 22:54:57 +0300

    Upate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "aaa.dimserk.androidlab"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "aaa.dimserk.androidlab"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- nativelib/build.gradle.kts
+++ nativelib/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "aaa.dimserk.nativelib"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 28
